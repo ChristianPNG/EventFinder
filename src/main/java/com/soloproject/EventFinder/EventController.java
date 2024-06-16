1d1906a6ba69db0a75e9c4475e777da70d2b7ac0
Christian R <rullan@udel.edu>
2024-06-16 18:13:35 -0400

    added simple json handling on the search via city feature

--- src/main/java/com/soloproject/EventFinder/EventController.java
+++ src/main/java/com/soloproject/EventFinder/EventController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+
+import org.json.*;
+
 @RestController
 public class EventController {
     public String url = "https://app.ticketmaster.com/discovery/v2/suggest?apikey=SuUqmKq5wDB7PbLtxlPKf1VQBiVA91Bo";
@@ -22,6 +26,15 @@ public class EventController {
     @GetMapping("/submitCity")
     public String submitCity(@RequestParam("CityName") String cityName, Model model){
         String URL = this.cityURL + cityName + "&" + this.API;
-        return restTemplate.getForObject(URL, String.class);
+        String jsonData = restTemplate.getForObject(URL, String.class);
+        JSONObject obj = new JSONObject(jsonData).getJSONObject("_embedded");
+        JSONArray arr = obj.getJSONArray("events");
+
+        ArrayList<String> events = new ArrayList<>();
+        for (int i = 0; i<20; i++){
+            events.add(arr.getJSONObject(i).getString("name"));
+        }
+        System.out.println(events);
+        return jsonData;
     }
 }
