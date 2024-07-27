e92106fba9ba20304b03b4931709394926d35416
Christian R <rullan@udel.edu>
2024-07-27 07:07:12 -0400

    configured post to handle json input + removed need for ID setter

--- Backend/src/main/java/com/soloproject/EventFinder/User.java
+++ Backend/src/main/java/com/soloproject/EventFinder/User.java
@@ -1,22 +1,30 @@
 package com.soloproject.EventFinder;
 import lombok.*;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import jakarta.persistence.*;
 
+
 @Entity
-@Data
-@Table(name = "USER")
+@Data //creates getters and setters for every field
+@Table(name = "USERS")
 public class User {
+
     @Id
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
     @Column(name = "USERNAME")
     private String name;
 
     @Column(name = "PASSWORD")
-    private String email;
+    private String password;
 
-    // getters and setters
+    public void setPassword(String password){
+        String encoded = DigestUtils.sha256Hex(password); //64 length
+        this.password = encoded;
+    }
+
 }
--- Backend/src/main/java/com/soloproject/EventFinder/UserController.java
+++ Backend/src/main/java/com/soloproject/EventFinder/UserController.java
@@ -2,6 +2,8 @@ package com.soloproject.EventFinder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,8 +13,17 @@ public class UserController {
     @Autowired
     private UserRepository UserRepo;
 
+    //GET USERS
     @GetMapping("/Users")
     public List<User> listAll() {
         return UserRepo.findAll();
+    }
+
+    @PostMapping("/register")
+    public void createUser(@RequestBody User user){
+        //Expects a user JSON object via a post request. User will be sent to the database
+        user.setName(user.getName());
+        user.setPassword(user.getPassword());
+        UserRepo.save(user);
     }
 }
